b027646f986f7aeb087ecf5171f2fca704bfdc11
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-09-17 15:40:32 +0200

    change

--- iredmail/runMinimal.sh
+++ iredmail/runMinimal.sh
@@ -1,14 +1,4 @@
 #!/bin/bash 
-sudo docker run \
-    --rm \
+sudo docker run \    
     --name iredmail \
-    -p 80:80 \
-    -p 443:443 \
-    -p 110:110 \
-    -p 995:995 \
-    -p 143:143 \
-    -p 993:993 \
-    -p 25:25 \
-    -p 465:465 \
-    -p 587:587 \
     ieasy:0.1

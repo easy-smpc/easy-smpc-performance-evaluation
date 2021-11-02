7df40ccb553df31b53583c982ba23222b0c33859
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-11-02 11:45:17 +0100

    add to local repo

---
+++ addToRepo.bat
@@ -0,0 +1,7 @@
+mvn install:install-file^
+  -Dfile=C:\Users\wirthfn\git\easy-smpc-performance-evaluation\src\main\resources\easysmpc.jar^
+  -DgroupId=org.bihealth.mi^
+  -DartifactId=easysmpc^
+  -Dversion=0.0.1-SNAPSHOT^
+  -Dpackaging=jar^
+  -DgeneratePom=true

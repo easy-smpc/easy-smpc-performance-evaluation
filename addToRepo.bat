2f12c114398336173d2a99b1fb4399c37b783801
Wirth, Felix Nikolaus <felix-nikolaus.wirth@charite.de>
2021-11-02 11:46:35 +0100

    Adapt repo

--- addToRepo.bat
+++ addToRepo.bat
@@ -1,5 +1,5 @@
 mvn install:install-file^
-  -Dfile=C:\Users\wirthfn\git\easy-smpc-performance-evaluation\src\main\resources\easysmpc.jar^
+  -Dfile=..\src\main\resources\easysmpc.jar^
   -DgroupId=org.bihealth.mi^
   -DartifactId=easysmpc^
   -Dversion=0.0.1-SNAPSHOT^
